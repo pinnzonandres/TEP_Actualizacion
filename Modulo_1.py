dfdb914b92db98a89702ca539e72928bb2bf76fc
Andres Pinzon <pinnzonandres@gmail.com>
2023-08-18 18:07:10 -0500

    Corrección Modulo 1

--- Modulo_1.py
+++ Modulo_1.py
@@ -182,6 +182,7 @@ def clean_general(df: pd.DataFrame, DIP: bool = False) -> pd.DataFrame:
     
     #  Agrupamos el dataframe por las columnas `Index_Rubro` y `Recurso` para tener el valor general para cada rubro según su recurso
     RG = RG.groupby(['Index_Rubro','Recurso'], as_index = False).agg({
+        'RUBRO':'first',
         'Dirección':'first',            # Tomamos la dirección asociado al rubro
         'Descripción Rubro' : 'first',  # Tomamos el programa asociado al rubro
         'APR. INICIAL' : 'sum',         # Sumamos la apropiación inicial
@@ -207,11 +208,15 @@ def clean_general(df: pd.DataFrame, DIP: bool = False) -> pd.DataFrame:
         RG['% EJECUCIÓN (C/A)'] = RG.apply(lambda x: round(int(x['PAGOS'])/int(x['APR. VIGENTE']),4), axis = 1)
     
         # Seleccionamos las columnas que necesitamos
+        RG = RG.rename
         RG = RG[['RUBRO','Descripción Rubro','APR. INICIAL','APR. REDUCIDA','APR. VIGENTE','CDP','COMPROMISO',
                  '% COMPROMETIDO (B/A)','APR. DISPONIBLE (A-B)','OBLIGACION','PAGOS','% EJECUCIÓN (C/A)']]
     
         # Renombramos las columnas que necesitamos
         RG = RG.rename(columns={'Descripción Rubro':'DESCRIPCION','APR. VIGENTE':'APR. VIGENTE (A)','COMPROMISO':'COMPROMISO (B)', 'PAGOS':'PAGOS (C)'})
+    
+    else:
+        RG = RG.drop(columns=['RUBRO'], axis = 1)
 
     # Devolvemos el dataframe limpio y agrupado
     return RG
